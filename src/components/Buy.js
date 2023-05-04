9429dd2721fa9d39a2ed1a24cded70b8eabeaaaf
Mohini250202 <124538787+Mohini250202@users.noreply.github.com>
2023-05-04 11:38:55 +0530

    first commit

---
+++ src/components/Buy.js
@@ -0,0 +1,65 @@
+import React, { useState ,} from 'react'
+import { ethers } from "ethers";
+const Buy = ({ state }) => {
+  const buyChai = async (event) => {
+    event.preventDefault();
+    const { contract } = state;
+    const name = document.querySelector("#name").value;
+    const message = document.querySelector("#message").value;
+    console.log(name, message, contract);
+    const amount = { value: ethers.utils.parseEther(text) };
+    const transaction = await contract.buyChai(name, message, amount);
+    await transaction.wait();
+    console.log("Transaction is done");
+  };
+  const onchange = (event) => {
+    settext(event.target.value);
+}
+const [text, settext] = useState("");
+
+  return (
+    <>
+      <div className="container-md" style={{ width: "50%", marginTop: "25px" }}>
+        <form onSubmit={buyChai}>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Enter Your Name"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Product name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="message"
+              placeholder="Enter Product name"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Amount</label>
+            <input
+              type="float"
+              className="form-control"
+              id="message"
+              placeholder="Enter Amount"
+             value={text} onChange={onchange}
+            />
+          </div>
+          
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={!state.contract}
+          >
+            Pay
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default Buy;
